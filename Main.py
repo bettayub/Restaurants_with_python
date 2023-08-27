df04f19b5beb08b6d85d0a55f9942a827fc84b52
bettayub <bettayub19@gmail.com>
2023-08-28 00:04:20 +0300

    Generate reviews for restaurants and customers

--- Main.py
+++ Main.py
@@ -8,3 +8,9 @@ restaurant2 = Restaurant("Kwa Mathe Fishpoint")
 
 customer1 = Customer("John", "kamau")
 customer2 = Customer("Musyoka", "Brian")
+
+# Creating reviews
+review1 = Review(customer1, restaurant1, 4)
+review2 = Review(customer2, restaurant1, 5)
+review3 = Review(customer1, restaurant2, 3)
+
