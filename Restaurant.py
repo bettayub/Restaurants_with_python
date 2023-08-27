c28589951861661e875440bbd8c1df0d55d54b72
bettayub <bettayub19@gmail.com>
2023-08-27 23:15:15 +0300

    Adding methods for getting restaurant name and calculating average star rating

--- Restaurant.py
+++ Restaurant.py
@@ -2,3 +2,14 @@ class Restaurant:
     def __init__(self, name):
         self.name = name
         self.reviews = []
+
+
+    def get_name(self):
+        return self.name
+
+    def average_star_rating(self):
+        if not self.reviews:
+            return 0
+        total_ratings = sum(review.get_rating() for review in self.reviews)
+        return total_ratings / len(self.reviews)
+
