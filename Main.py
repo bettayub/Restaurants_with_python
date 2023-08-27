9aa289910cf14dafb19142263c9d54bf1d0d81c6
bettayub <bettayub19@gmail.com>
2023-08-28 00:03:02 +0300

    creating  Customer instances: 'John kamau' and 'Musyoka Brian'

--- Main.py
+++ Main.py
@@ -5,3 +5,6 @@ from Review import Review
 # Creating instances
 restaurant1 = Restaurant("Chips Palace")
 restaurant2 = Restaurant("Kwa Mathe Fishpoint")
+
+customer1 = Customer("John", "kamau")
+customer2 = Customer("Musyoka", "Brian")
