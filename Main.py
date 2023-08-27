bef3dc55511bc2604001b89a7c7121d6f91cd857
bettayub <bettayub19@gmail.com>
2023-08-28 00:07:18 +0300

    Displaying o

--- Main.py
+++ Main.py
@@ -14,11 +14,9 @@ review1 = Review(customer1, restaurant1, 4)
 review2 = Review(customer2, restaurant1, 5)
 review3 = Review(customer1, restaurant2, 3)
 
-
 # Displaying average ratings
 print(f"Average rating for {restaurant1.get_name()}: {restaurant1.average_star_rating()}")
 print(f"Average rating for {restaurant2.get_name()}: {restaurant2.average_star_rating()}")
-
 
 # Displaying customer reviews
 for customer in [customer1, customer2]:
@@ -26,3 +24,7 @@ for customer in [customer1, customer2]:
     for review in customer.reviews:
         print(f"- {review.get_rating()} stars for {review.get_restaurant().get_name()}")
 
+# Displaying all reviews
+print("All reviews:")
+for review in Review.all():
+    print(f"- {review.get_customer().given_name} reviewed {review.get_restaurant().get_name()} with {review.get_rating()} stars")
